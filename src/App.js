867f198027c09b555ce7ec5b1702272dad8e9d0d
masoume <masoumevsl198@gmail.com>
2020-03-07 00:59:54 +0330

    start card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import Card from './Components/Card';
+import { Row, Col } from 'antd';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Row gutter={16}>
+      <Col className="gutter-row" span={6}>
+        <Card text="salam" />
+      </Col>
+      <Col className="gutter-row" span={6}>
+      <Card text="salam" />
+      </Col>
+      <Col className="gutter-row" span={6}>
+      <Card text="salam" />
+      </Col>
+      <Col className="gutter-row" span={6}>
+      <Card text="salam" />
+      </Col>
+    </Row>
     </div>
   );
 }
