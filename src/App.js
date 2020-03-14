b1bbf99f95d60824d8bdaa88e5c0647778c43332
masoume <masoumevsl198@gmail.com>
2020-03-14 11:53:55 +0330

    start project taxi-online with react.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import './App.css';
 import Card from './Components/Card';
 import { Row, Col } from 'antd';
+
+
 function App() {
   return (
     <div className="App">
-          <Row gutter={16}>
-      <Col className="gutter-row" span={6}>
-        <Card text="salam" />
-      </Col>
-      <Col className="gutter-row" span={6}>
-      <Card text="salam" />
-      </Col>
-      <Col className="gutter-row" span={6}>
-      <Card text="salam" />
-      </Col>
-      <Col className="gutter-row" span={6}>
-      <Card text="salam" />
-      </Col>
-    </Row>
+      <Row id="back">
+          <Col span={18} push={6} className="part-right">
+            <h3>سامانه درخواست تاکسی آنلاین</h3>
+          </Col>
+          <Col span={6} pull={18} className="card">
+            اپ تاکسی آنلاین
+          </Col>
+        </Row>,
     </div>
   );
 }
